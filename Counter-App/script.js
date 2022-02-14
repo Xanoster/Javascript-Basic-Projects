ff897cc3621ef941c3de03102da112f295e239b4
Xanoster <techypankaj@gmail.com>
2022-02-14 18:06:00 +0100

    Add counter functionality

---
+++ Counter-App/script.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+const counter = document.getElementById('counter');
+const decreaseBtn = document.getElementById('decrease');
+const resetBtn = document.getElementById('reset');
+const increaseBtn = document.getElementById('increase');
+
+decreaseBtn.addEventListener('click', () => {
+    count--;
+    updateCounter();
+});
+
+resetBtn.addEventListener('click', () => {
+    count = 0;
+    updateCounter();
+});
+
+increaseBtn.addEventListener('click', () => {
+    count++;
+    updateCounter();
+});
+
+function updateCounter() {
+    counter.textContent = count;
+
+    if (count > 0) {
+        counter.style.color = '#27ae60';
+    } else if (count < 0) {
+        counter.style.color = '#e74c3c';
+    } else {
+        counter.style.color = '#667eea';
+    }
+}
