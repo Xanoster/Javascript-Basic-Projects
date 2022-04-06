f7e2153589130f0731c254ba0aed6bd4a7d9ba55
Xanoster <techypankaj@gmail.com>
2022-04-06 16:42:00 +0200

    feat: implement number appending logic

---
+++ Simple-Calculator/script.js
@@ -0,0 +1,40 @@
+const resultDisplay = document.getElementById('result');
+
+function appendNumber(number) {
+    if (resultDisplay.value === '0') {
+        resultDisplay.value = number;
+    } else {
+        resultDisplay.value += number;
+    }
+}
+
+function appendOperator(operator) {
+    const lastChar = resultDisplay.value.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        resultDisplay.value = resultDisplay.value.slice(0, -1) + operator;
+    } else {
+        resultDisplay.value += operator;
+    }
+}
+
+function clearResult() {
+    resultDisplay.value = '0';
+}
+
+function deleteLast() {
+    resultDisplay.value = resultDisplay.value.slice(0, -1);
+    if (resultDisplay.value === '') {
+        resultDisplay.value = '0';
+    }
+}
+
+function calculate() {
+    try {
+        resultDisplay.value = eval(resultDisplay.value);
+    } catch (error) {
+        resultDisplay.value = 'Error';
+        setTimeout(() => clearResult(), 1500);
+    }
+}
+
+// feat: implement number appending logic
